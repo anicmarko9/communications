f3a6a9efb25597e250111da8263ca5efad84cd39
Marko Anic <anicmarko9>
2025-06-14 14:17:51 +0200

    test: Email RegExp

--- internal/utils/regexp_test.go
+++ internal/utils/regexp_test.go
@@ -2,8 +2,8 @@ package utils
 
 import "testing"
 
-// The structure for phone number validation test cases.
-type phoneTestCase struct {
+// The structure for RegExp validation test cases.
+type regexpTestCase struct {
 	name  string
 	input string
 	want  bool
@@ -11,7 +11,7 @@ type phoneTestCase struct {
 
 // Checks if the input string matches the expected phone number format.
 func TestValidatePhoneNumber(t *testing.T) {
-	tests := []phoneTestCase{
+	tests := []regexpTestCase{
 		{"Valid phone number", "+12345678901", true},
 		{"Missing plus sign", "1234567890", false},
 		{"Too short (less than 10 digits)", "+123", false},
@@ -23,6 +23,25 @@ func TestValidatePhoneNumber(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ValidatePhoneNumber(tt.input); got != tt.want {
 				t.Errorf("ValidatePhoneNumber(%q) = %v, want %v", tt.input, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidateEmail(t *testing.T) {
+	tests := []regexpTestCase{
+		{"Valid email", "test@example.com", true},
+		{"Missing @ symbol", "test.example.com", false},
+		{"Missing domain", "test@", false},
+		{"Invalid characters", "test@exa!mple.com", false},
+		{"Multiple @ symbols", "test@@example.com", false},
+		{"Missing local part", "@example.com", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ValidateEmail(tt.input); got != tt.want {
+				t.Errorf("ValidateEmail(%q) = %v, want %v", tt.input, got, tt.want)
 			}
 		})
 	}
